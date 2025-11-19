130abe24f0ecad8ba0bb55e376dfb04fde989ff9
Abhay-Raghav <abhayraghuvanshi0001@gmail.com>
2025-11-19 15:01:34 +0530

    init

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    
+    @Test
+    public void testMain() {
+        Assertions.assertTrue(true);   
+    }
+
+
+}
