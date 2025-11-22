a12a121a4d12c96090d1d0e046fbf2a9420ef677
Akshat Singh <akhatsingh2212@gmail.com>
2025-11-22 22:15:11 +0530

    kuch kar diya hu

--- src/main/java/com/adv/hacker/App.java
+++ src/main/java/com/adv/hacker/App.java
@@ -1,6 +1,19 @@
 package com.adv.hacker;
-
-// hello
 public class App {
-    
+    public static void main(String[] args) {
+    String name ="akshat";
+    String name2= name.replace('a','b');
+    System.out.println(name2);
+    int age = 25;
+    String name3 = "akshat";
+System.out.println(age);
+System.out.println(name3);
+int [] marks=new int[3];
+marks[1]=52;
+marks[0]=65;
+marks[2]=52;
+
+System.out.println(marks[0]);
+    }
 }
+ 
