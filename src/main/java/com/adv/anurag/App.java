0e79d270e69cb9200be216ad119fbbe380b1bc35
ANURAG-PATHAKK <anuragpathak6994@gmail.com>
2025-11-24 13:12:16 +0530

    hi bhaiyaa

--- src/main/java/com/adv/anurag/App.java
+++ src/main/java/com/adv/anurag/App.java
@@ -3,5 +3,7 @@ package com.adv.anurag;
 
 // hacker ne mera laptop kharab kiya hai
 public class App {
-    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
